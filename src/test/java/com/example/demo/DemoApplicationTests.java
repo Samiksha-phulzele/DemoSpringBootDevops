7cf168ad398fe444274e091bc4c84dcbc7ced675
Phulzele <samiksha.phulzele@fisglobal.com>
2021-11-14 14:35:33 +0530

    Map to latest code

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -13,7 +13,7 @@ class DemoApplicationTests {
 		log.info("************In master branch************");
 		log.info("************In develop branch************");
 		log.info("************In feature branch updated some app level code , this branch is cut off from develop branch************");
-		log.info("**********Release1.0 branch have added new features is cut off from again develop branch then this branch will merge to develop and master");
+		log.info("**********Release branch have added new features is cut off from again develop branch then this branch will merge to develop and master");
 		log.info("There bug in application so hotfix branch cut off from master and fixed the bug, Again will merge back to develop and master");
 	}
 
